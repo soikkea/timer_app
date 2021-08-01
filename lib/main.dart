7d1cd2c5a5c05cd35e9b75243add9e57282f384c
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2021-08-01 15:30:27 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'package:timer_app/homepage.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(TimerApp());
 }
 
-class MyApp extends StatelessWidget {
+class TimerApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Timer App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: "Timer App"),
     );
   }
 }
